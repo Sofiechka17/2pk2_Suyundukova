2f41a7a47aea7bb0705f356866cc967678c364e6
София Суюндукова <sofiasa2006@mail.ru>
2023-09-22 14:40:40 +0500

    практика 5

---
+++ PZ_05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое положительное число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int i = 0;
+
+            do
+            {
+                sum += i;
+                i++;
+            }
+            while (i <= n);
+
+            Console.WriteLine("Сумма всех целых чисел от 0 до {0} равна {1}", n, sum);
+        }
+    }
+}
