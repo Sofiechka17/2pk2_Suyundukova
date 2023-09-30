988ba09cd12fc28495777fe541cce65ed493ef6c
София Суюндукова <sofiasa2006@mail.ru>
2023-09-30 09:13:07 +0500

    практика 7

---
+++ PZ_07/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк матрицы: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов матрицы: ");
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, columns];
+
+            Console.WriteLine("Введите элементы матрицы:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write("Элемент [{0},{1}]: ", i, j);
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Исходная матрица:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            // Сортировка столбцов по убыванию значений элементов в первой строке
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows - 1; i++)
+                {
+                    for (int k = i + 1; k < rows; k++)
+                    {
+                        if (matrix[i, j] < matrix[k, j])
+                        {
+                            int temp = matrix[i, j];
+                            matrix[i, j] = matrix[k, j];
+                            matrix[k, j] = temp;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Матрица после сортировки столбцов:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
