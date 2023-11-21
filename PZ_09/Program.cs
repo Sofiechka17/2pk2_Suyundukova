fadbf0cc1fed82e3321f9761539d53473df71de7
София Суюндукова <sofiasa2006@mail.ru>
2023-11-21 15:16:50 +0500

    практика 10

--- PZ_09/Program.cs
+++ PZ_09/Program.cs
@@ -44,6 +44,7 @@ namespace PZ_09
         }
     }
 }
+    
               
 
 
---
+++ PZ_10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            int sum = 0; 
+            int count = 0; 
+            int i = 0;
+
+            while (i < text.Length)
+            {
+                string number = ""; // текущее число
+
+                // проверяем, является ли символ цифрой 
+                while (i < text.Length && (Char.IsDigit(text[i])))
+                {
+                    number += text[i];
+                    i++;
+                }
+
+                if (!String.IsNullOrEmpty(number))
+                {
+                    sum += int.Parse(number);
+                    count++;
+                }
+
+                i++;
+            }
+
+            double average = (double)sum / count;
+
+            Console.WriteLine($"Среднее значение чисел: {average}");
+        }
+    }
+}
